763bbd2e03be1a5949d835d2ec18fc9d23b7d0e9
fayyazfz <fayyazshaikfz@gmail.com>
2025-02-17 23:54:35 -0500

    added ignore file

---
+++ Chapter_4_FlowControl/Lab11.java
@@ -0,0 +1,17 @@
+public class Lab11 {
+    public static void main(String args[]) {
+        for (int k = 1; k <= 4; k++) {
+            for (int i = 1; i <= 8; i++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+            for (int j = 1; j <= 8; j++) {
+                System.out.print(" *");
+            }
+
+
+            System.out.println();
+        }
+    }
+}
